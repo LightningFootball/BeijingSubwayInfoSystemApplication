fc77e2cb6e3629b7a73b1c305b6fbf752010ace0
李泽润 <lizerun1999@outlook.com>
2020-10-12 14:29:36 +0800

    Qt test. Base info added.

--- main.cpp
+++ main.cpp
@@ -1,10 +1,14 @@
 #include "BeijingSubwayInfoSystenApplication.h"
 #include <QtWidgets/QApplication>
+#include <qlabel.h>
+#include <QtWidgets/qpushbutton.h>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     BeijingSubwayInfoSystenApplication w;
     w.show();
+    QLabel* label = new QLabel("Hello World!");
+    label->show();
     return a.exec();
 }
