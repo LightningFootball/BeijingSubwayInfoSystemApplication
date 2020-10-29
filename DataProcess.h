87289fba4883905ff7f5e76bc90cef851bcd4333
李泽润 <lizerun1999@outlook.com>
2020-10-29 22:02:15 +0800

    The exchange info of the last station of  loop line cant resolve correctly.

--- DataProcess.h
+++ DataProcess.h
@@ -1,35 +1,5 @@
 #pragma once
 
-
-
-#define LINENUM(NUM)\
-QString aLineSTRING##NUM = infoFile.readLine();\
-QString aLineSTRING_TRIMMED##NUM = aLineSTRING##NUM.trimmed();\
-QStringList readLineSpilted##NUM = aLineSTRING_TRIMMED##NUM.split(' ');\
-stationHead line##NUM;\
-line##NUM.lineNum = readLineSpilted##NUM.at(0).toInt();\
-cout << line##NUM.lineNum << endl;\
-line##NUM.lineName = readLineSpilted##NUM.at(1);\
-cout << line##NUM.lineName << endl;\
-line##NUM.totalStationNum = readLineSpilted##NUM.at(2).toInt();\
-cout << line##NUM.totalStationNum << endl;\
-QVector<station> stationList##NUM(line##NUM.totalStationNum);\
-for (int i = 3; i < readLineSpilted##NUM.count(); i++)\
-{\
-	if (readLineSpilted##NUM.at(i).toInt() > 0 && readLineSpilted##NUM.at(i).toInt() < 99999)\
-	{\
-		stationList##NUM[(i - 2) / 2 - 1].nextStationDistance = readLineSpilted##NUM.at(i).toInt();\
-	}\
-	else\
-	{\
-		stationList##NUM[(i - 1) / 2 - 1].stationName = readLineSpilted##NUM.at(i);\
-	}\
-}\
-for (int i = 0; i < stationList##NUM.count(); i++)\
-{\
-	cout << stationList##NUM[i].stationName << stationList##NUM[i].nextStationDistance << endl;\
-}\
-
 #include <string>
 #include <fstream>
 #include <vector>
@@ -39,92 +9,96 @@ for (int i = 0; i < stationList##NUM.count(); i++)\
 #include<qfile.h>
 #include<qtextstream.h>
 
-//#pragma execution_character_set("utf-8")
-
-
 //head for test
 #include <iostream>
 
 using namespace std;
 
-struct stationHead
-{
-	QString lineName;
-	int lineNum;
-	int totalStationNum;
-	int* stationPtr = nullptr;
-};
-
 struct station
 {
 	QString stationName;
-	int nextStationDistance = 0;
-	int* exchangeListPtr = nullptr;
+	//QStringList stationNameSplit;
+	struct exchangeStation* next = NULL;
 };
 
-struct exchangeList
+struct exchangeStation
 {
-	int* exchangeLine1 = nullptr;
-	int* exchangeLine2 = nullptr;
+	int stationNum=-1;
+	int distance=0;
+	struct exchangeStation* next = NULL;
 };
 
 int dataProcess() {
 	QFile infoFile("SubwayInfoDatabase.txt");
 	infoFile.open(QIODevice::ReadOnly);		//open file
-	QTextStream cout(stdout, QIODevice::WriteOnly);
 
-	QString totalLineNumSTRING;
-	totalLineNumSTRING = infoFile.readLine();
+	
+	QString totalLineNumSTRING = infoFile.readLine();
 	int totalLineNum = totalLineNumSTRING.toInt();
 
+	QTextStream cout(stdout, QIODevice::WriteOnly);		//allow qstring could cout
+	cout << totalLineNum << endl;	//console
+	
+	QVector<station> stationList;
 	for (int i = 0; i < totalLineNum; i++)
 	{
-		LINENUM(i);
+		QString aLineSTRING = infoFile.readLine();
+		QString aLineSTRING_TRIMMED = aLineSTRING.trimmed();		//tail trimmed
+		QStringList readLineSpilted = aLineSTRING_TRIMMED.split(' ');		//spilt by space
+
+		int lineNum = readLineSpilted.at(0).toInt();
+		cout << lineNum << endl;	//console
+		QString lineName = readLineSpilted.at(1);
+		cout << lineName << endl;	//console
+		int lineTotalStationNum = readLineSpilted.at(2).toInt();
+		cout << lineTotalStationNum << endl;
+
+		for (int i = 3; i < readLineSpilted.size(); i = i + 2)
+		{
+			station curStation;
+			curStation.stationName = readLineSpilted.at(i);
+			//curStation.stationNameSplit = curStation.stationName.split("");
+			
+			exchangeStation curExchangeStation;
+			if (readLineSpilted.at(i + 1).toInt()!=0)
+			{	
+				curStation.next = &curExchangeStation;
+				curExchangeStation.distance = readLineSpilted.at(i + 1).toInt();
+				curExchangeStation.stationNum = stationList.size() + 1;
+			}
+			else if ( (readLineSpilted.at(i + 1).toInt() != 0) && (i>=readLineSpilted.size()-10) )  //error
+			{
+				curExchangeStation.stationNum =999 /*stationList.size()-lineTotalStationNum+2*/;
+			}
+			else if (readLineSpilted.at(i + 1).toInt() == 0)
+			{
+
+			}
+
+			stationList.append(curStation);			
+			
+			cout<< stationList.size()-1 <<' '<< curStation.stationName << ' ' /*<< curStation.stationNameSplit.size() << ' '*/ << curExchangeStation.stationNum << ' ' << curExchangeStation.distance << endl;
+		}
 	}
+	
+	
+
+
+	//for (int i = 3; i < readLineSpilted.count(); i++)
+	//{
+	//	if (readLineSpilted.at(i).toInt()>0&&readLineSpilted.at(i).toInt()<99999)	//if num
+	//	{
+	//		stationList[(i - 2) / 2 - 1].nextStationDistance = readLineSpilted.at(i).toInt();
+	//	}
+	//	else
+	//	{
+	//		stationList[(i - 1) / 2 - 1].stationName = readLineSpilted.at(i);
+	//	}
+	//}
+	//for (int i = 0; i < stationList.count(); i++)
+	//{
+	//	cout << stationList[i].stationName << stationList[i].nextStationDistance << endl;
+	//}
 
 	return 0;
 }
-
-//int dataProcess() {
-//	QFile infoFile("SubwayInfoDatabase.txt");
-//	infoFile.open(QIODevice::ReadOnly);		//open file
-//
-//	QString totalLineNumSTRING;
-//	totalLineNumSTRING = infoFile.readLine();	//read first line as qstring
-//	int totalLineNum = totalLineNumSTRING.toInt(nullptr, 10);	//qstring to int
-//
-//	QTextStream cout(stdout, QIODevice::WriteOnly);		//allow qstring could cout
-//	cout << totalLineNum << endl;	//console
-//
-//	QString aLineSTRING = infoFile.readLine();
-//	QString aLineSTRING_TRIMMED = aLineSTRING.trimmed();		//tail trimmed
-//	QStringList readLineSpilted = aLineSTRING_TRIMMED.split(' ');		//spilt by space
-//
-//	stationHead line1;
-//	line1.lineNum = readLineSpilted.at(0).toInt();
-//	cout << line1.lineNum << endl;	//console
-//	line1.lineName = readLineSpilted.at(1);
-//	cout << line1.lineName << endl;	//console
-//	line1.totalStationNum = readLineSpilted.at(2).toInt();
-//	cout << line1.totalStationNum << endl;
-//	
-//
-//	QVector<station> stationList(line1.totalStationNum);
-//	for (int i = 3; i < readLineSpilted.count(); i++)
-//	{
-//		if (readLineSpilted.at(i).toInt()>0&&readLineSpilted.at(i).toInt()<99999)	//if num
-//		{
-//			stationList[(i - 2) / 2 - 1].nextStationDistance = readLineSpilted.at(i).toInt();
-//		}
-//		else
-//		{
-//			stationList[(i - 1) / 2 - 1].stationName = readLineSpilted.at(i);
-//		}
-//	}
-//	for (int i = 0; i < stationList.count(); i++)
-//	{
-//		cout << stationList[i].stationName << stationList[i].nextStationDistance << endl;
-//	}
-//
-//	return 0;
-//}
